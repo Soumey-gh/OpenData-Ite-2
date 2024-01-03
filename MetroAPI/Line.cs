6db35a466bdebfd4a99243cfdf118b8dfb448a27
Soumeya GHALEM <soumeya.ghalem@Digital-Grenoble.local>
2024-01-03 13:35:11 +0100

    Library push

---
+++ MetroAPI/Line.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenDataAPI
+
+{
+    public class LineTransport
+    {
+        [JsonProperty("id")]
+        public string id { get; set; }
+
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("lon")]
+        public double lon { get; set; }
+
+        [JsonProperty("lat")]
+        public double lat { get; set; }
+
+        [JsonProperty("zone")]
+        public string zone { get; set; }
+
+        [JsonProperty("lines")]
+        public List<string> lines { get; set; }
+    }
+}
---
+++ MetroAPI/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace OpenDataAPI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MetroAPI result = new MetroAPI();
+            List<LineTransport> afficherLine = result.GetLine(); 
+       
+            // Réalisation d'un Foreach pour pouvoir afficher toutes les informations pour chaque lignes de la liste: 
+            foreach (LineTransport line in afficherLine)
+            {
+                Console.WriteLine($"{line.id}: {line.name}:{line.zone}:{line.lon}{line.lat}:{line.lines}");
+                //Console.WriteLine(line.id + ": " + line.name + ":" + line.zone + ":" + line.lon +line.lat + ":" + line.lines);
+            }
+           
+        }
+
+    }
+ }
+
